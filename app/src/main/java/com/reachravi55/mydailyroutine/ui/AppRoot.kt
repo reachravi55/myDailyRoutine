699357203d7fcfafb6d18174b38b6b6cc604f3db
Ravi Vendra <reachravi55@gmail.com>
2026-01-08 10:16:58 -0500

    new files 4

---
+++ app/src/main/java/com/reachravi55/mydailyroutine/ui/AppRoot.kt
@@ -0,0 +1,16 @@
+package com.reachravi55.mydailyroutine.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import com.reachravi55.mydailyroutine.data.RoutineRepository
+import com.reachravi55.mydailyroutine.proto.RoutineStore
+
+@Composable
+fun AppRoot() {
+    val ctx = LocalContext.current
+    val repo = RoutineRepository.get(ctx)
+    val store by repo.flow().collectAsState(initial = RoutineStore.getDefaultInstance())
+    AppScaffold(store = store)
+}
